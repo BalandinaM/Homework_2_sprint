b32b3d2ae61751cbd3e3fa905b4b79f58ddc5446
BalandinaM <marina.balandina.89@mail.ru>
2026-03-09 22:39:05 +0500

    init, HW1, HW2

---
+++ hws2-master/src/s2-homeworks/hw02/HW2.tsx
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import Affairs from './affairs/Affairs'
+import s2 from '../../s1-main/App.module.css'
+
+/*
+* 1 - описать типы AffairPriorityType, AffairType// ok
+* 2 - указать нужный тип для defaultAffairs// ok 
+* 3 - дописать типы и логику функции filterAffairs и проверить её тестами
+* 4 - выполнить пункт 3 для функции deleteAffair
+* 5 - указать нужный тип в useState с affairs
+* 6 - дописать тип и логику функции deleteAffairCallback
+* 7 - в файле Affairs.tsx дописать типизацию пропсов
+* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
+* 9 - в файле Affair.tsx дописать типизацию пропсов
+* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
+* 11 - в файле Affair.tsx отобразить приходящие данные
+* */
+
+// types
+export type AffairPriorityType = 'high' | 'middle' | 'low';
+export type AffairType = {
+    _id: number;
+    name: string; 
+    priority: AffairPriorityType
+}
+export type FilterType = 'all' | AffairPriorityType
+
+// constants
+const defaultAffairs: AffairType[] = [
+    {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
+    {_id: 2, name: 'anime', priority: 'low'},
+    {_id: 3, name: 'games', priority: 'low'},
+    {_id: 4, name: 'work', priority: 'high'},
+    {_id: 5, name: 'html & css', priority: 'middle'},
+]
+
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => {
+    if (filter === 'all') {
+        return affairs
+    }
+    return affairs.filter(affair => affair.priority === filter)
+}
+
+export const deleteAffair = (affairs: AffairType[], _id: AffairType['_id']): AffairType[] => {
+    return affairs.filter(affair => affair._id !== _id) 
+}
+
+function HW2() {
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs)
+    const [filter, setFilter] = useState<FilterType>('all')
+
+    const filteredAffairs = filterAffairs(affairs, filter)
+
+    const deleteAffairCallback = (_id: AffairType['_id']) => {
+        const newState = deleteAffair(affairs, _id);
+        setAffairs(newState);
+    }
+
+    return (
+        <div id={'hw2'}>
+            <div className={s2.hwTitle}>Homework #2</div>
+            <div className={s2.hw}>
+                <Affairs
+                    data={filteredAffairs}
+                    setFilter={setFilter}
+                    deleteAffairCallback={deleteAffairCallback}
+                    filter={filter}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default HW2
