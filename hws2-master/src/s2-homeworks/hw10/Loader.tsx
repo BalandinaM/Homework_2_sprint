4d5d28306051244ffeba6475b05374c265294471
BalandinaM <marina.balandina.89@mail.ru>
2026-05-07 12:55:49 +0500

    HW10

--- hws2-master/src/s2-homeworks/hw10/Loader.tsx
+++ hws2-master/src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,13 @@
 import s from './Loader.module.css'
+import loaderImage from './loader.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+  return (
+    <img
+      src={loaderImage}
+      alt="Loading..."
+      className={s.loader}
+      style={{ width: '50px', height: '50px' }}
+    />
+  )
+}
--- hws2-master/src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ hws2-master/src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,31 @@
-const initState = {
-    isLoading: false,
+export type initStateType = {
+  isLoading: boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+const initState: initStateType = {
+  isLoading: false,
+}
 
-        default:
-            return state
-    }
+export const loadingReducer = (
+  state: initStateType = initState,
+  action: LoadingActionType
+): initStateType => {
+  switch (action.type) {
+    case 'CHANGE_LOADING':
+      console.log('run!')
+      return { ...state, isLoading: action.isLoading }
+
+    default:
+      return state
+  }
 }
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
+  type: 'CHANGE_LOADING'
+  isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
+  type: 'CHANGE_LOADING',
+  isLoading,
 })
