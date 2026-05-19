bcb26557e0f715321a434258aefc824e78ebc444
BalandinaM <marina.balandina.89@mail.ru>
2026-05-19 13:29:28 +0500

    HW12

--- hws2-master/src/s2-homeworks/hw12/bll/themeReducer.ts
+++ hws2-master/src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,27 @@
 const initState = {
-    themeId: 1,
+  themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
-
-        default:
-            return state
-    }
+export type Theme = {
+  themeId: number
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export type changeThemeIdAT = ReturnType<typeof changeThemeId>
+export type ActionType = changeThemeIdAT
+
+export const themeReducer = (
+  state: Theme = initState,
+  action: ActionType
+): Theme => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case 'SET_THEME_ID':
+      return { ...state, themeId: action.id }
+    default:
+      return state
+  }
+}
+
+export const changeThemeId = (id: number) =>
+  ({ type: 'SET_THEME_ID', id }) as const // fix any
